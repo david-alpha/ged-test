0e005e264efe2116f9676e6c21d24dd2ce4e05e1
root <david.lacan@aresformation.com>
2021-02-05 09:13:19 +0000

    envoi du 502 - session

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -15,7 +15,7 @@ class AuthentificationController extends AbstractController
     public function index(): Response
     {
         return $this->render('authentification/index.html.twig', [
-            'controller_name' => 'AuthentificationController',
+            'controller_name' => 'Bienvenue sur notre super plateforme.',
         ]);
     }
 	#[Route('/insertUser', name: 'insertUser')]
@@ -41,5 +41,41 @@ class AuthentificationController extends AbstractController
         return $this->render('authentification/insertUser.html.twig', [
             'controller_name' => "Ajout en base de données.",
         ]);
+    }
+	#[Route('/connexion', name: 'connexion')]
+    public function connexion(Request $request, EntityManagerInterface $manager): Response
+    {
+		//Récupération des identifiants de connexion
+		$identifiant = $request->request->get('login');
+		$password    = $request->request->get('password');
+		//Test de l'existence d'un tel couple
+		$aUser = $manager->getRepository(Utilisateur::class)->findBy(["nom"=>$identifiant, "code"=>$password]);
+		if (sizeof($aUser)>0){
+			$utilisateur = new Utilisateur;
+            $utilisateur = $aUser[0];
+			//démarrage des variables de session
+			$sess = $request->getSession();
+			//Information de session
+			$sess->set("idUtilisateur", $utilisateur->getId());
+			$sess->set("nomUtilisateur", $utilisateur->getNom());
+			$sess->set("prenomUtilisateur", $utilisateur->getPrenom());
+			
+			return $this->redirectToRoute('dashboard');
+		}else{
+			return $this->redirectToRoute('authentification');
+		}
+		dd($identifiant, $password, $reponse);
+		return new response(1);
+        // return $this->render('authentification/insertUser.html.twig', [
+            // 'controller_name' => "Ajout en base de données.",
+        // ]);
+    }
+	#[Route('/dashboard', name: 'dashboard')]
+    public function dashboard(Request $request, EntityManagerInterface $manager): Response
+    {
+		$sess = $request->getSession();
+        return $this->render('authentification/dashboard.html.twig',[
+         'controller_name' => "Espace Client",
+         ]);
     }
 }
